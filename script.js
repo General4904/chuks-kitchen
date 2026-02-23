7101ea45588cc9edff7e0a5158ab29b65bd1e558
Generalissimo4904 <koyejoibukun@gmail.com>
2026-02-23 23:01:17 +0100

    Completed the explore page

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log(`Hello World`);
+
+document.addEventListener("DOMContentLoaded", () => {
+  // Select all forms on Sign In or Sign Up pages
+  const authForms = document.querySelectorAll(".login-container form");
+
+  authForms.forEach((form) => {
+    form.addEventListener("click", (e) => {
+      e.preventDefault(); // Prevent the actual "login" attempt
+      window.location.href = "categories.html"; // Go straight to the food!
+    });
+  });
+});
